82c985e3ba8bc85b285a889de6deaeff3445e95d
Samuel Yuan <samuelyuan@users.noreply.github.com>
2018-07-08 22:57:10 +0000

    Move shader code to a separate file

--- src/render/main.go
+++ src/render/main.go
@@ -10,32 +10,12 @@ import (
 	"math"
 	"os"
 	"runtime"
-	"strings"
 )
 
 const (
 	windowWidth      = 800
 	windowHeight     = 600
 	MouseSensitivity = 0.05
-
-	vertexShaderSource = `
-		#version 410
-	  layout (location = 0) in vec3 position;
-
-    uniform mat4 view;
-
-		void main() {
-			gl_Position = view * vec4(position, 1.0);
-		}
-	` + "\x00"
-
-	fragmentShaderSource = `
-		#version 410
-		out vec4 frag_colour;
-		void main() {
-			frag_colour = vec4(1, 1, 1, 1.0);
-		}
-	` + "\x00"
 )
 
 var (
@@ -174,29 +154,6 @@ func initGLFW() *glfw.Window {
 	return window
 }
 
-func compileShader(source string, shaderType uint32) (uint32, error) {
-	shader := gl.CreateShader(shaderType)
-
-	csources, free := gl.Strs(source)
-	gl.ShaderSource(shader, 1, csources, nil)
-	free()
-	gl.CompileShader(shader)
-
-	var status int32
-	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("Failed to compile %v: %v", source, log)
-	}
-
-	return shader, nil
-}
-
 func initOpenGL() uint32 {
 	if err := gl.Init(); err != nil {
 		panic(err)
@@ -209,22 +166,8 @@ func initOpenGL() uint32 {
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
 
-	// compile shaders
-	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
-	if err != nil {
-		panic(err)
-	}
-	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	programShader := gl.CreateProgram()
-	gl.AttachShader(programShader, vertexShader)
-	gl.AttachShader(programShader, fragmentShader)
-	gl.LinkProgram(programShader)
-
-	return programShader
+	shader := NewShader()
+	return shader.ProgramShader
 }
 
 func makeVertexArrayObj(vertices []float32) uint32 {
---
+++ src/render/shader.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"strings"
+)
+
+const (
+	vertexShaderSource = `
+		#version 410
+	  layout (location = 0) in vec3 position;
+
+    uniform mat4 view;
+
+		void main() {
+			gl_Position = view * vec4(position, 1.0);
+		}
+	` + "\x00"
+
+	fragmentShaderSource = `
+		#version 410
+		out vec4 frag_colour;
+		void main() {
+			frag_colour = vec4(1, 1, 1, 1.0);
+		}
+	` + "\x00"
+)
+
+type Shader struct {
+	VertexShader   uint32
+	FragmentShader uint32
+	ProgramShader  uint32
+}
+
+func NewShader() *Shader {
+	sh := Shader{}
+
+	// compile shaders
+	sh.VertexShader = sh.initVertexShader()
+	sh.FragmentShader = sh.initFragmentShader()
+
+	programShader := gl.CreateProgram()
+	gl.AttachShader(programShader, sh.VertexShader)
+	gl.AttachShader(programShader, sh.FragmentShader)
+	gl.LinkProgram(programShader)
+	sh.ProgramShader = programShader
+
+	return &sh
+}
+
+func (sh *Shader) compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("Failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+func (sh *Shader) initVertexShader() uint32 {
+	vertexShader, err := sh.compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	return vertexShader
+}
+
+func (sh *Shader) initFragmentShader() uint32 {
+	fragmentShader, err := sh.compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	return fragmentShader
+}
